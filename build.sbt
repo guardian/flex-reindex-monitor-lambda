64a8115eafe363bd6d33bdab85cca5bd4004f1ae
Paul Roberts <pmr@stelo.org.uk>
2018-03-22 16:13:17 +0000

    read an event from the stream

--- build.sbt
+++ build.sbt
@@ -8,6 +8,8 @@ version := "1.0"
 
 scalaVersion := "2.12.4"
 
+resolvers += Resolver.bintrayRepo("guardian", "editorial-tools")
+
 scalacOptions ++= Seq(
   "-deprecation",
   "-encoding", "UTF-8",
@@ -20,7 +22,8 @@ val awsLambdaVersion = "1.2.0"
 libraryDependencies ++= Seq(
   "com.amazonaws" % "aws-lambda-java-core" % awsLambdaVersion,
   "com.amazonaws" % "aws-lambda-java-events" % awsLambdaVersion,
-  "org.slf4j" % "slf4j-simple" % "1.7.25"
+  "org.slf4j" % "slf4j-simple" % "1.7.25",
+  "com.gu" %% "flexible-model" % "0.0.13",
 )
 
 enablePlugins(RiffRaffArtifact)
--- src/main/scala/com/gu/flexiblecontent/reindexmonitor/Lambda.scala
+++ src/main/scala/com/gu/flexiblecontent/reindexmonitor/Lambda.scala
@@ -2,9 +2,16 @@ package com.gu.flexiblecontent.reindexmonitor
 
 import scala.collection.JavaConverters._
 
+import java.io.ByteArrayInputStream
+import java.nio.ByteBuffer
+import org.apache.thrift.protocol.TCompactProtocol
+import org.apache.thrift.transport.TIOStreamTransport
+import scala.util.{ Try, Success, Failure }
+
 import com.amazonaws.services.lambda.runtime.{ Context, RequestHandler }
 import org.slf4j.{ Logger, LoggerFactory }
 import com.amazonaws.services.lambda.runtime.events.KinesisEvent
+import com.gu.flexiblecontent.model.thrift.Event
 
 /**
  * This is compatible with aws' lambda JSON to POJO conversion.
@@ -23,24 +30,40 @@ object Env {
     Option(System.getenv("Stage")).getOrElse("DEV"))
 }
 
+object ThriftDeserialiser {
+  def deserialiseEvent(input: ByteBuffer): Try[Event] = {
+    Try {
+      val bis = new ByteArrayInputStream(input.array());
+      val transport = new TIOStreamTransport(bis)
+      val protocol = new TCompactProtocol(transport)
+      Event.decode(protocol)
+    }
+  }
+}
+
 object Lambda extends RequestHandler[KinesisEvent, Unit] {
 
   val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
-  /*
-   * This is your lambda entry point
-   */
   def handleRequest(input: KinesisEvent, context: Context): Unit = {
     val env = Env()
     logger.info(s"Starting $env")
     process(input, env)
   }
 
-  /*
-   * I recommend to put your logic outside of the handler
-   */
   def process(data: KinesisEvent, env: Env): Unit = {
-    val records = data.getRecords().asScala
-    logger.info(s"Recieved: ${records.length} record(s)")
+    val records = data.getRecords().asScala.map(_.getKinesis())
+    for (rec <- records) {
+      ThriftDeserialiser.deserialiseEvent(rec.getData()) match {
+        case Success(ev) => logger.info(s"decoded event: $ev")
+        case Failure(err) => logger.info(s"failed: $err")
+      }
+    }
+  }
+}
+
+object TestMain {
+  def main(args: Array[String]) = {
+    println("main test")
   }
 }
